cc46e16ae47878e871bd4a4955cf7d2f7c82e532
Patrick Killian <pkillian@berkeley.edu>
2013-08-04 14:29:32 -0400

    Updated cachebuster; added font awesome icons; added social media list

--- components/js/pkillianme.js
+++ components/js/pkillianme.js
@@ -3,7 +3,7 @@ var pkill = {};
 (function(that, $, window, undefined) {
   "use strict";
 
-  this.cacheBust = "2013-08-04-02";
+  this.cacheBust = "2013-08-04-05";
 
   this.setCurrentSection = function($section) {
     $section.parent().find('.current').removeClass('current');
