8d84c8df65b3cdaa13760441ca81bbabb9a28add
Patrick Killian <pkillian@berkeley.edu>
2013-08-04 11:13:53 -0400

    Changed formatting of blogposts; add first-line indent; update cachebust variable

--- components/js/pkillianme.js
+++ components/js/pkillianme.js
@@ -3,7 +3,7 @@ var pkill = {};
 (function(that, $, window, undefined) {
   "use strict";
 
-  this.cacheBust = "2013-07-18-01";
+  this.cacheBust = "2013-08-04-02";
 
   this.setCurrentSection = function($section) {
     $section.parent().find('.current').removeClass('current');
