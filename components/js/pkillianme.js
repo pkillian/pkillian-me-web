917343b323882e357e0f47d0c2ba7a97e80956f2
Patrick Killian <pkillian@berkeley.edu>
2014-01-22 14:51:20 -0800

    Revise placement of Google Analytics code

--- components/js/pkillianme.js
+++ components/js/pkillianme.js
@@ -70,11 +70,3 @@ $(document).ready(function() {
   pkill.bindSidebarFold();
 });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-47387011-1', 'pkillian.me');
-ga('send', 'pageview');
-
